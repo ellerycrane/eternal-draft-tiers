07a358315ab8917a0dbe63966827c6963989ef5a
Ellery Crane <ellery@pwnieexpress.com>
2017-03-05 12:33:48 -0500

    Initial functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,186 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import './compiled-css/index.css'
 
-class App extends Component {
+import GoogleSpreadsheetsParser from './googleSpreadsheetsParser'
+import Fuse from './fuse'
+
+const spreadsheetUrl = "https://docs.google.com/spreadsheets/d/18n-9kwej73Up0R4zF2nGdKFEAQ3LT_D2FdqDlu7jTU0/pubhtml",
+  sheetTitle = 'draft_tier_list'
+
+const ranks = [
+  'S',
+  "A+",
+  "A",
+  "A-",
+  "B+",
+  "B",
+  "B-",
+  "C+",
+  "C",
+  "C-",
+  "D+",
+  "D",
+  "D-",
+  "F",
+  "F-",
+]
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+  }
+
+  handleFilterTextInputChange(e) {
+    this.props.onFilterTextInput(e.target.value);
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          onChange={this.handleFilterTextInputChange}
+        />
+      </form>
     );
   }
 }
 
-export default App;
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      rawColumns: null,
+      rankedCards: null,
+      filteredCards: null,
+      filterText: ''
+    }
+  }
+
+  componentDidMount() {
+    this.gss = new GoogleSpreadsheetsParser(spreadsheetUrl, {sheetTitle: sheetTitle, hasTitle: true})
+    console.log(this.parseRanks(this.gss.columns));
+    let rankedCards = this.parseRanks(this.gss.columns)
+    var options = {
+      shouldSort: true,
+      tokenize: true,
+      matchAllTokens: true,
+      threshold: 0.1,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: [
+        "cardName"
+      ]
+    };
+    this.fuse = new Fuse(rankedCards, options); // "list" is the item array
+
+    window.GSS = this.gss
+    console.log(JSON.stringify(rankedCards, null, 2))
+    this.setState({rawColumns: this.gss.columns, rankedCards: rankedCards, filteredCards: rankedCards})
+  }
+
+  handleFilterTextInput(filterText) {
+    console.log("FILTERING with text", filterText)
+    let result = this.state.rankedCards
+    if (filterText && filterText !== '') {
+      result = this.fuse.search(filterText);
+    }
+
+    this.setState({
+      filterText: filterText,
+      filteredCards: result
+    })
+  }
+  scrubRank(rank) {
+    return (rank || '').replace('-', '-minus').replace('+', '-plus').toLowerCase()
+  }
+
+  parseRanks(columns) {
+    let currentRank = null,
+      cardRanks = [],
+      cardNames = []
+
+    columns.forEach((col, colIdx)=> {
+      currentRank = null
+      col.forEach((row, rowIdx)=> {
+        if (ranks.indexOf(row) !== -1) {
+          currentRank = row
+        } else if (row !== null && row !== '' && currentRank !== null && cardNames.indexOf(row) === -1) {
+          cardRanks.push({cardName: row, rank: currentRank})
+        }
+      })
+    })
+    return cardRanks
+  }
+
+  makeSafeForCSS(name) {
+    return name.replace(/[^a-z0-9]/g, function (s) {
+      var c = s.charCodeAt(0);
+      if (c == 32) return '-';
+      if (c >= 65 && c <= 90) return '_' + s.toLowerCase();
+      return '__' + ('000' + c.toString(16)).slice(-4);
+    });
+  }
+
+
+  render() {
+    let cols = []
+    if (this.state.rawColumns) {
+      this.state.rawColumns.forEach((col, colIdx)=> {
+        let columnRows = []
+        col.forEach((row, rowIdx)=> {
+          columnRows.push(<li key={'cell-' + colIdx + '-' + rowIdx}>{row}</li>)
+        })
+        cols.push(
+          <ul key={colIdx}>
+            {columnRows}
+          </ul>
+        )
+      })
+    }
+
+    let cards = [],
+      cssData = []
+    if (this.state.filteredCards) {
+      this.state.filteredCards.forEach((card)=> {
+        cards.push(
+          <div className={'card ' + this.makeSafeForCSS(card.cardName) + ' ' + this.scrubRank(card.rank)} key={card.cardName}>
+            <div className={'card-rank '+this.scrubRank(card.rank)}>{card.rank}</div>
+          </div>
+        )
+        // cards.push(<img class="grid-card-img animate" src={"http://www.numotgaming.com/cards/images/cards/"+card.cardName+".png"} />)
+        cssData.push({
+          cardName: card.cardName,
+          className: this.makeSafeForCSS(card.cardName),
+          imageUrl: "http://www.numotgaming.com/cards/images/cards/" + card.cardName + ".png"
+        })
+      })
+    }
+    {/*<pre>*/}
+          {/*{cssData.map((d)=>{*/}
+            {/*return `.${d.className} {\nbackground-image: url("${d.imageUrl}");\n}\n\n`*/}
+          {/*})}*/}
+        {/*</pre>*/}
+    // cards = []
+    return (
+      <div className="App">
+        <SearchBar
+          filterText={this.state.filterText}
+          onFilterTextInput={this.handleFilterTextInput.bind(this)}
+        />
+
+        <div className="cards">
+          {cards}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
